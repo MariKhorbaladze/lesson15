94ed92136f0ccfd1c6ebd118e3739ecf2bc38811
MariKhorbaladze <129887035+MariKhorbaladze@users.noreply.github.com>
2024-04-25 14:19:52 +0400

    lesson15

---
+++ script.js
@@ -0,0 +1,47 @@
+// work 1
+
+var userName = "Mari";
+let adress = "Tbilisi";
+let age = 5;
+const isAvailable = "true";
+console.log(userName, adress, age, isAvailable)
+
+
+//work 2
+
+let x = 13;
+let y = 23;
+let summ = x + y;
+console.log(summ);
+
+let n = 133;
+let m = 450;
+let multiplication = n * m;
+console.log(multiplication);
+
+let s = 12300;
+let g = 4500;
+let difference = s - g;
+console.log(difference);
+
+let a = 13;
+let r = 123;
+let division = a / r;
+console.log(division);
+
+//work 3
+
+let item = 3400;
+let person = 3500;
+console.log("Hello we have only " + item + " medicine and if you want we can send it for your " + person + " human.");
+console.log('Hello our ${item} medicine may help your ${person} human.');
+
+//work 4 
+
+let lenght = 5;
+let width = 7;
+let area = lenght * width;
+console.log(area);
+
+
+
